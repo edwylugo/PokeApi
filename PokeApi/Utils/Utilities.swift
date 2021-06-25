cc854f57a01998f35749839540fddf0efe1ed709
Edwy Lugo <edwylugo@gmail.com>
2021-06-25 20:33:34 -0300

    Create Utilities

---
+++ PokeApi/Utils/Utilities.swift
@@ -0,0 +1,13 @@
+//
+//  Utilities.swift
+//  PokeApi
+//
+//  Created by Edwy Lugo on 25/06/21.
+//
+
+import UIKit
+
+class Utilities {
+    let sectionInsets = UIEdgeInsets(top: 15, left: 15, bottom: 15, right: 15)
+    let itemsPerRow: CGFloat = 2
+}
