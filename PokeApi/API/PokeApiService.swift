173ca07c12283196b26d335631cb3baccda8a29a
Edwy Lugo <edwylugo@gmail.com>
2021-06-25 16:20:21 -0300

    Create Service API

--- PokeApi/API/PokeApiService.swift
+++ PokeApi/API/PokeApiService.swift
@@ -8,4 +8,26 @@
 import Alamofire
 import AlamofireObjectMapper
 
-
+class PokeApiService {
+    static let shared = PokeApiService()
+    
+    //MARK: - Get Pokemons
+    func fetchPokemons(name: String?, id: Int, page: Int = 0, onComplete: @escaping (PokemonList?) -> Void) {
+        let url: String
+        url = "\(API_BASE)\(LIMIT)\(ITEMS)\(OFFSET)\(page)"
+        print("DEBUG: url...: \(url)")
+        
+        Alamofire.request(url).responseObject { (response: DataResponse<PokemonList>) in
+            onComplete(response.result.value)
+        }
+    }
+    
+    //MARK: - Get Pokemon Info
+    func fetchPokemonInfo(name: String?, id: Int, onComplete: @escaping (PokemonDetail?) -> Void) {
+        let url = API_BASE + "\(name!)/"
+        print("DEBUG: url...: \(url)")
+        Alamofire.request(url).responseObject { (response: DataResponse<PokemonDetail>) in
+            onComplete(response.result.value)
+        }
+    }
+}
--- PokeApi/Utils/Constants.swift
+++ PokeApi/Utils/Constants.swift
@@ -9,3 +9,7 @@ import Foundation
 
 //MARK: = Account Information
 let API_BASE = "https://pokeapi.co/api/v2/pokemon/"
+let ITEMS = 30
+let LIMIT = "?limit="
+let OFFSET = "&offset="
+
