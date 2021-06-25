90aa1528fb744d9185d2c28cb43fb873419bc2c0
Edwy Lugo <edwylugo@gmail.com>
2021-06-25 16:07:15 -0300

    Prepare API and Service

---
+++ PokeApi/API/PokeApiService.swift
@@ -0,0 +1,11 @@
+//
+//  PokeApiService.swift
+//  PokeApi
+//
+//  Created by Edwy Lugo on 25/06/21.
+//
+
+import Alamofire
+import AlamofireObjectMapper
+
+
---
+++ PokeApi/Utils/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  PokeApi
+//
+//  Created by Edwy Lugo on 25/06/21.
+//
+
+import Foundation
+
+//MARK: = Account Information
+let API_BASE = "https://pokeapi.co/api/v2/pokemon/"
